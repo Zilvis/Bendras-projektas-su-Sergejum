43a0a5c23bd7c05d8a777a9d023f2ba4afa7e362
zilvis92 <zi.daugela@gmail.com>
2024-07-15 18:39:20 +0300

    initial

---
+++ src/main/java/dev/zilvis/Bendras/projektas/su/Sergejum/BendrasProjektasSuSergejumApplication.java
@@ -0,0 +1,13 @@
+package dev.zilvis.Bendras.projektas.su.Sergejum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BendrasProjektasSuSergejumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BendrasProjektasSuSergejumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/zilvis/Bendras/projektas/su/Sergejum/BendrasProjektasSuSergejumApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zilvis.Bendras.projektas.su.Sergejum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BendrasProjektasSuSergejumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
