8d07a8900cc105a168fdcd8a102186012c9839fe
zilvis92 <zi.daugela@gmail.com>
2024-07-22 17:45:46 +0300

    Perdaryta

---
+++ src/main/java/dev/zilvis/Bendras/projektas/su/Sergejum/Enums/CatList.java
@@ -0,0 +1,12 @@
+package dev.zilvis.Bendras.projektas.su.Sergejum.Enums;
+
+public enum CatList {
+    Transportas,
+    Nekilnojamas_Turtas,
+    Darbai,
+    Paslaugos,
+    Buitis,
+    Kompiuterija,
+    Komunikacijos,
+
+}
